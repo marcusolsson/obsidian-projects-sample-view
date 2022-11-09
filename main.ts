e01a597dd0b3ed33d44b1fc306b19fb3d57a4ec0
Marcus Olsson <8396880+marcusolsson@users.noreply.github.com>
2022-11-09 16:22:24 +0100

    Convert to Sample View

--- main.ts
+++ main.ts
@@ -1,137 +1,67 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from "obsidian";
+import {
+	DataQueryResult,
+	ProjectView,
+	ProjectViewProps,
+} from "obsidian-projects-types";
 
-// Remember to rename these classes and interfaces!
+class MySampleView extends ProjectView {
+	dataEl?: HTMLElement;
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+	getViewType(): string {
+		return "my-sample-view";
+	}
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	getDisplayName(): string {
+		return "Sample view";
+	}
+
+	getIcon(): string {
+		return "apple";
+	}
+
+	// onData is called whenever the data has been updated (for whatever reason).
+	// Whenever this function is called, you should invalidate previous data.
+	//
+	// `data`        Contains the parsed data.
+	async onData({ data }: DataQueryResult) {
+		if (this.dataEl) {
+			this.dataEl.empty();
+			this.dataEl.createDiv({ text: JSON.stringify(data.fields) });
+			this.dataEl.createDiv({ text: JSON.stringify(data.records) });
+		}
+	}
+
+	// onOpens is called whenever the user activates your view.
+	//
+	// `contentEl`    HTML element where you can attach your view.
+	// `config`       JSON object with optional view configuration.
+	// `saveConfig`   Callback to save configuration changes.
+	// `readonly`     If true, you should disable any UI features that updates the
+	//                underlying data. Currently, readonly is only true for
+	//                Dataview projects, where fields may be computed.
+	async onOpen({
+		contentEl,
+		config,
+		saveConfig,
+		readonly,
+	}: ProjectViewProps) {
+		console.log("Opening ", this.getDisplayName());
+
+		contentEl.createEl("h1", { text: "My Sample View" });
+
+		this.dataEl = contentEl.createEl("div");
+	}
+
+	// onOpens is called whenever the user leaves or removes your view.
+	// Use this to clean up any resources you've created.
+	async onClose() {
+		console.log("Closing ", this.getDisplayName());
+	}
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+	// This method is called by Projects to register your view. The Projects
+	// plugin may call this function multiple times to create the view.
+	onRegisterProjectView = () => new MySampleView();
 }
